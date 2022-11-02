763952050e4a9e158cd4d098d50e17b26efba6b4
jyueong <uj9125@gmail.com>
2022-11-02 17:07:38 +0900

    upload 01-03

---
+++ 03_vue/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="border">
+    <h1>싸피 이거는 내가 만든 새로운 컴포넌트다!</h1>
+    <MyComponentItem
+    static-props="MyComponent에서 보낸 데이터"
+    :dynamic-props="dynamicProps"
+    @oh-my-god="parentGetEvent"
+    @child-input="getDynamicData"/>
+  </div>
+</template>
+
+<script>
+// 1. 불러오기
+import MyComponentItem from '@/components/MyComponentItem'
+
+export default {
+  name: 'MyComponent',
+  components: {
+    // 2. 등록하기
+    MyComponentItem,
+  },
+  data: function () {
+    return {
+      dynamicProps: '이건 동적인 데이터입니다!',
+    }
+  },
+  methods: {
+    parentGetEvent: function (childData) {
+      console.log("오마이갓")
+      console.log(childData)
+    },
+    getDynamicData: function (childInputData) {
+      console.log(`사용자가 입력한 값은 ${childInputData}입니다.`)     
+    }
+  }
+}
+</script>
+
+<style>
+  .border {
+    border: solid 1px black;
+  }
+</style>
---
+++ 03_vue_prac/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="border">
+    <h1>우왕 이거는 내가 만든 새로운 컴포넌트다!</h1>
+    <!-- 3. 보여주기 -->
+    <MyComponentItem/>
+  </div>
+</template>
+
+<script>
+// 1. 불러오기
+import MyComponentItem from '@/components/MyComponentItem'
+
+export default {
+  name: 'MyComponent',
+  components: {
+    // 2. 등록하기
+    MyComponentItem,
+  }
+}
+</script>
+
+<style>
+  .border {
+    border: solid 1px black;
+  }
+</style>
